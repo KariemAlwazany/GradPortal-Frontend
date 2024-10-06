e3185538c83497068ef3dc53626156cdf1b27fef
KariemAbuAisheh <kariemalwazany@gmail.com>
2024-10-07 00:13:43 +0300

    DevicePreview

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,28 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_project/screens/welcome_screen.dart';
 import 'package:flutter_project/theme/theme.dart';
+import 'package:device_preview/device_preview.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    DevicePreview(
+      enabled: !kReleaseMode,
+      builder: (context) => const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Student's Hub",
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
       theme: lightMode,
       home: WelcomeScreen(),
     );
