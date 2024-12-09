3537168e686663fc1b6919b398da8da49e7e42d0
KariemAbuAisheh <kariemalwazany@gmail.com>
2024-12-09 23:42:53 +0200

    maps done and shoppp management 90%

--- lib/screens/Shop/update_seller_profile_screen.dart
+++ lib/screens/Shop/update_seller_profile_screen.dart
@@ -1,12 +1,12 @@
-// ignore_for_file: library_private_types_in_public_api
-
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_project/screens/Shop/update_user_student_screen.dart';
 import 'package:line_awesome_flutter/line_awesome_flutter.dart';
 import 'package:http/http.dart' as http;
-import 'dart:convert';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
 
 class UpdateProfileScreen extends StatefulWidget {
   const UpdateProfileScreen({super.key});
@@ -22,14 +22,20 @@ class _UpdateProfileScreenState extends State<UpdateProfileScreen> {
   final TextEditingController phoneNumberController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
   final TextEditingController confirmPasswordController = TextEditingController();
-  final TextEditingController shopNameController = TextEditingController(); // New field
+  final TextEditingController shopNameController = TextEditingController();
+  final TextEditingController locationController = TextEditingController();
 
+  String location = 'Loading...';
   String username = 'Loading...';
   String fullName = 'Loading...';
   String email = 'Loading...';
   String phoneNumber = 'Loading...';
   String role = 'Loading...';
-  String shopName = 'Loading...'; // New field
+  String shopName = 'Loading...';
+
+  LatLng? userLocation;
+  LatLng? selectedLocation;
+  late GoogleMapController mapController;
 
   @override
   void initState() {
@@ -53,12 +59,12 @@ class _UpdateProfileScreenState extends State<UpdateProfileScreen> {
           role = "Not logged in";
           phoneNumber = "Not logged in";
           fullName = "Not logged in";
-          shopName = "Not logged in"; // New field
+          shopName = "Not logged in";
+          location = "Not logged in";
         });
         return;
       }
 
-      // Fetch Role Data
       final roleResponse = await http.get(
         roleUrl,
         headers: {
@@ -83,7 +89,6 @@ class _UpdateProfileScreenState extends State<UpdateProfileScreen> {
         });
       }
 
-      // Fetch Profile Data
       final profileResponse = await http.get(
         userUrl,
         headers: {
@@ -95,18 +100,21 @@ class _UpdateProfileScreenState extends State<UpdateProfileScreen> {
         final profileData = json.decode(profileResponse.body);
         setState(() {
           phoneNumber = profileData['Phone_number'] ?? "No phone number found";
-          shopName = profileData['Shop_name'] ?? "No shop name found"; // New field
+          shopName = profileData['Shop_name'] ?? "No shop name found";
+          location = "${profileData['city']} ${profileData['longitude']} ${profileData['latitude']}" ?? "Location not found";
         });
       } else {
         setState(() {
           phoneNumber = "Error loading phone number";
-          shopName = "Error loading shop name"; // New field
+          shopName = "Error loading shop name";
+          location = "Error loading location";
         });
       }
     } catch (e) {
       setState(() {
         phoneNumber = "Error loading data";
-        shopName = "Error loading data"; // New field
+        shopName = "Error loading data";
+        location = "Error loading location";
       });
     }
   }
@@ -124,7 +132,6 @@ class _UpdateProfileScreenState extends State<UpdateProfileScreen> {
       return;
     }
 
-    // Check password and confirm password match
     if (passwordController.text.isNotEmpty &&
         passwordController.text != confirmPasswordController.text) {
       ScaffoldMessenger.of(context).showSnackBar(
@@ -133,14 +140,13 @@ class _UpdateProfileScreenState extends State<UpdateProfileScreen> {
       return;
     }
 
-    // Build the data object with non-empty fields
     Map<String, dynamic> updates = {};
     if (usernameController.text.isNotEmpty) updates['Username'] = usernameController.text;
     if (phoneNumberController.text.isNotEmpty) updates['Phone_number'] = phoneNumberController.text;
     if (fullNameController.text.isNotEmpty) updates['FullName'] = fullNameController.text;
     if (emailController.text.isNotEmpty) updates['Email'] = emailController.text;
     if (passwordController.text.isNotEmpty) updates['Password'] = passwordController.text;
-    if (shopNameController.text.isNotEmpty) updates['Shop_name'] = shopNameController.text; // New field
+    if (shopNameController.text.isNotEmpty) updates['Shop_name'] = shopNameController.text;
 
     try {
       final response = await http.patch(
@@ -156,7 +162,7 @@ class _UpdateProfileScreenState extends State<UpdateProfileScreen> {
         ScaffoldMessenger.of(context).showSnackBar(
           const SnackBar(content: Text('Profile updated successfully')),
         );
-        fetchUserData(); // Refresh data after successful update
+        fetchUserData();
       } else {
         final errorData = json.decode(response.body);
         ScaffoldMessenger.of(context).showSnackBar(
@@ -167,25 +173,175 @@ class _UpdateProfileScreenState extends State<UpdateProfileScreen> {
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(content: Text('Network error')),
       );
-      print(e);
     }
+  }
+
+  Future<void> _getUserLocation() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Location services are disabled.')),
+      );
+      return;
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Location permissions are denied.')),
+        );
+        return;
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+            content: Text(
+                'Location permissions are permanently denied. We cannot access location.')),
+      );
+      return;
+    }
+
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    setState(() {
+      userLocation = LatLng(position.latitude, position.longitude);
+      selectedLocation = userLocation;
+    });
+  }
+
+  Future<void> _saveLocationToApi() async {
+    if (selectedLocation == null) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Please select a location.')),
+      );
+      return;
+    }
+
+    final latitude = selectedLocation!.latitude;
+    final longitude = selectedLocation!.longitude;
+
+    try {
+      final baseUrl = dotenv.env['API_BASE_URL'] ?? '';
+      final locationUrl = Uri.parse('$baseUrl/GP/v1/users/updateUserLocation');
+      final prefs = await SharedPreferences.getInstance();
+      final token = prefs.getString('jwt_token');
+
+      if (token == null) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('User not logged in.')),
+        );
+        return;
+      }
+
+      final response = await http.patch(
+        locationUrl,
+        headers: {
+          'Authorization': 'Bearer $token',
+          'Content-Type': 'application/json',
+        },
+        body: json.encode({
+          'latitude': latitude,
+          'longitude': longitude,
+        }),
+      );
+
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        final city = data['data']['user']['city'];
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Location updated successfully: $city')),
+        );
+      } else {
+        final error = json.decode(response.body);
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Error: ${error['message']}')),
+        );
+      }
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Failed to update location.')),
+      );
+    }
+  }
+
+  void _showMapDialog() async {
+    await _getUserLocation();
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return StatefulBuilder(
+          builder: (context, setDialogState) {
+            return AlertDialog(
+              title: const Text('Select Location'),
+              content: SizedBox(
+                width: double.maxFinite,
+                height: 400,
+                child: userLocation == null
+                    ? const Center(child: CircularProgressIndicator())
+                    : GoogleMap(
+                        initialCameraPosition: CameraPosition(
+                          target: userLocation!,
+                          zoom: 15,
+                        ),
+                        onMapCreated: (controller) => mapController = controller,
+                        myLocationEnabled: true,
+                        markers: selectedLocation != null
+                            ? {
+                                Marker(
+                                  markerId: const MarkerId('selectedLocation'),
+                                  position: selectedLocation!,
+                                ),
+                              }
+                            : {},
+                        onTap: (LatLng tappedPoint) {
+                          setDialogState(() {
+                            selectedLocation = tappedPoint;
+                          });
+                        },
+                      ),
+              ),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text('Close'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    _saveLocationToApi();
+                    Navigator.pop(context);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xFF3B4280),
+                  ),
+                  child: const Text(
+                    'Save Location',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16.0,
+                    ),
+                  ),
+                ),
+              ],
+            );
+          },
+        );
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    bool isEditable = role != "Student" && role != "User"; // Only allow editing if the role is not "Student" or "User"
-
-    // Navigate to UpdateUserSellerProfileScreen if role is User or Student
-    if (role == "Student" || role == "User") {
-      Future.delayed(Duration.zero, () {
-        Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(
-            builder: (context) => const UpdateUserSellerProfileScreen(),
-          ),
-        );
-      });
-    }
+    bool isEditable = role != "Student" && role != "User";
 
     return Scaffold(
       appBar: AppBar(
@@ -208,109 +364,113 @@ class _UpdateProfileScreenState extends State<UpdateProfileScreen> {
         iconTheme: const IconThemeData(color: Color(0xFF3B4280)),
       ),
       body: SingleChildScrollView(
-        child: Container(
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            children: [
-              Stack(
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                SizedBox(
+                  width: 120,
+                  height: 120,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(100),
+                    child: Image.asset('assets/images/logo.png', fit: BoxFit.cover),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 10),
+            Text(
+              'Role: $role',
+              style: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                color: Color(0xFF3B4280),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Form(
+              child: Column(
                 children: [
+                  _buildTextField(
+                    controller: fullNameController,
+                    label: fullName,
+                    hint: fullName,
+                    icon: LineAwesomeIcons.user,
+                    isEnabled: isEditable,
+                  ),
+                  _buildTextField(
+                    controller: usernameController,
+                    label: username,
+                    hint: username,
+                    icon: LineAwesomeIcons.user,
+                    isEnabled: isEditable,
+                  ),
+                  _buildTextField(
+                    controller: emailController,
+                    label: email,
+                    hint: email,
+                    icon: LineAwesomeIcons.mail_bulk,
+                    isEnabled: isEditable,
+                  ),
+                  _buildTextField(
+                    controller: phoneNumberController,
+                    label: phoneNumber,
+                    hint: phoneNumber,
+                    icon: LineAwesomeIcons.phone,
+                    isEnabled: true,
+                  ),
+                  _buildTextField(
+                    controller: shopNameController,
+                    label: shopName,
+                    hint: shopName,
+                    icon: LineAwesomeIcons.store,
+                    isEnabled: isEditable,
+                  ),
+                  _buildTextField(
+                    controller: locationController,
+                    label: location,
+                    hint: location,
+                    icon: LineAwesomeIcons.map_pin,
+                    isEnabled: true,
+                  ),
+                  const SizedBox(height: 20),
+                  ElevatedButton.icon(
+                    onPressed: _showMapDialog,
+                    icon: const Icon(Icons.map, color: Colors.white),
+                    label: const Text(
+                      "Open Map",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFF3B4280),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
                   SizedBox(
-                    width: 120,
-                    height: 120,
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(100),
-                      child: Image.asset('assets/images/logo.png', fit: BoxFit.cover),
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: updateProfile,
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xFF3B4280),
+                      ),
+                      child: const Text(
+                        "Confirm Changes",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
                     ),
                   ),
                 ],
               ),
-              const SizedBox(height: 10),
-              Text(
-                'Role: $role',
-                style: const TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                  color: Color(0xFF3B4280),
-                ),
-              ),
-              const SizedBox(height: 20),
-              Form(
-                child: Column(
-                  children: [
-                    _buildTextField(
-                      controller: fullNameController,
-                      label: fullName,
-                      hint: fullName,
-                      icon: LineAwesomeIcons.user,
-                      isEnabled: isEditable,
-                    ),
-                    _buildTextField(
-                      controller: usernameController,
-                      label: username,
-                      hint: username,
-                      icon: LineAwesomeIcons.user,
-                      isEnabled: isEditable,
-                    ),
-                    _buildTextField(
-                      controller: emailController,
-                      label: email,
-                      hint: email,
-                      icon: LineAwesomeIcons.envelope_1,
-                      isEnabled: isEditable,
-                    ),
-                    _buildTextField(
-                      controller: phoneNumberController,
-                      label: phoneNumber,
-                      hint: phoneNumber,
-                      icon: LineAwesomeIcons.phone,
-                      isEnabled: true, // Always editable for all users
-                    ),
-                    _buildTextField(
-                      controller: shopNameController, // New field
-                      label: shopName, // New field
-                      hint: shopName, // New field
-                      icon: LineAwesomeIcons.store, // New field
-                      isEnabled: isEditable,
-                    ),
-                    _buildTextField(
-                      controller: passwordController,
-                      label: 'Password',
-                      hint: 'Enter new password',
-                      icon: LineAwesomeIcons.fingerprint,
-                      isPassword: true,
-                      isEnabled: isEditable,
-                    ),
-                    _buildTextField(
-                      controller: confirmPasswordController,
-                      label: 'Confirm Password',
-                      hint: 'Re-enter new password',
-                      icon: LineAwesomeIcons.fingerprint,
-                      isPassword: true,
-                      isEnabled: isEditable,
-                    ),
-                    const SizedBox(height: 20),
-                    SizedBox(
-                      width: double.infinity,
-                      child: ElevatedButton(
-                        onPressed: updateProfile,
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: const Color(0xFF3B4280),
-                        ),
-                        child: const Text(
-                          "Confirm Changes",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 20.0,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
